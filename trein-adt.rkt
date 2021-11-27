e6b3fef35b123748f8fcc0316f0b0826396f684e
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2021-11-27 16:40:28 +0100

    trein+treinreeks-adt

---
+++ trein-adt.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(require "simulator/interface.rkt")
+
+; We stellen een trein voor door een abstractie ervan te maken.
+(define (maak-trein id prev-seg cur-seg)
+  (let ((trein-id id)
+        (richting prev-seg) ; bepaald op basis van prev-seg
+        (startsegment cur-seg)
+        (snelheid 0))
+
+
+    ; snelheid van huidige trein
+    (define (huidige-snelheid)
+      snelheid)
+
+    ; verander snelheid van huidige trein
+    (define (verander-snelheid! nieuwe-snelheid)
+      (set! snelheid nieuwe-snelheid))
+
+    ; dispatch-procedure
+    (define (dispatch-trein msg)
+      (cond ((eq? msg 'trein-id) trein-id)
+            ((eq? msg 'richting) prev-seg)
+            ((eq? msg 'startsegment) startsegment)
+            ((eq? msg 'huidige-snelheid) snelheid)
+            ((eq? msg 'verander-snelheid!) verander-snelheid!)
+            (else (display "foute boodschap - trein-adt"))))
+    dispatch-trein))
