c0fe96e764c659a8aa89b329ee1dd39b38934c68
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2021-12-03 16:55:17 +0100

    temp-update

--- INFRABEL.rkt
+++ INFRABEL.rkt
@@ -1,8 +1,43 @@
 #lang racket
 
 (require "simulator/interface.rkt")
+(require "trein-adt.rkt")
+(require "treinreeks-adt.rkt")
+(require "wissel-adt.rkt")
+(require "spoornetwerk-adt.rkt")
+
+(provide maak-programma)
 
 
-(setup-straight)
-(start)
+(define (maak-programma)
+  (let ((spoor (maak-spoornetwerk)))
+
+
+    (define (start-programma-straight)
+      (setup-straight)
+      (start))
+
+    (define (start-programma-loop)
+      (setup-loop)
+      (start))
+
+    (define (start-programma-loop-and-switches)
+      (setup-loop-and-switches)
+      (start))
+
+    (define (start-programma-straight-with-switch)
+      (setup-straight-with-switch)
+      (start))
+
+
+    (define (dispatch-programma msg)
+      (cond ((eq? msg 'start-programma-straight) start-programma-straight)
+            ((eq? msg 'start-programma-loop) start-programma-loop)
+            ((eq? msg 'start-programma-loop-and-switches) start-programma-loop-and-switches)
+            ((eq? msg 'start-programma-straight-with-switch) start-programma-straight-with-switch)
+            (else (display "foute boodschap - INFRABEL"))))
+    dispatch-programma))
+
+
+
 
