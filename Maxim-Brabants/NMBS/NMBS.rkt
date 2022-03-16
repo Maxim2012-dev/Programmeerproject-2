e9f5058140e8ff218a8a16cf8487643829120e84
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-03-16 16:27:44 +0100

    temp

--- Maxim-Brabants/NMBS/NMBS.rkt
+++ Maxim-Brabants/NMBS/NMBS.rkt
@@ -5,8 +5,9 @@
 (require "wissel-adt.rkt")
 (require "spoornetwerk-adt.rkt")
 
-(import (rnrs base)
-        (a-d graph unweighted config))
+(require "a-d/graph/unweighted/config.rkt")
+(require "a-d/graph-algorithms/undirected/bft-applications.rkt")
+
 (provide maak-nmbs)
 
 (define SNELHEIDSVERANDERING 20)
@@ -15,20 +16,26 @@
 (define (maak-nmbs)
   (let ((spoor (maak-spoornetwerk)))
 
+    ;; vector met spoorcomponenten
+    (define componenten (vector 'S2 'D1 'D2 'D3 'D4 'D5 'D6 'D7 'D8 'D9 'S1 'S3))
 
     ;; graafrepresentatie van spooropstelling
+    ;; (we zien de detectieblokken en de wissels als nodes)
     (define spoornetwerk
-      (let ((g (new #f 8)))
-        (add-edge! g 0 2)
+      (let ((g (new #f 12)))
+        (add-edge! g 0 1)
+        (add-edge! g 0 6)
         (add-edge! g 0 7)
-        (add-edge! g 0 5)
-        (add-edge! g 2 6)
-        (add-edge! g 1 7)
-        (add-edge! g 5 3)
-        (add-edge! g 5 4)
+        (add-edge! g 1 2)
+        (add-edge! g 2 3)
         (add-edge! g 3 4)
-        (add-edge! g 4 6)
-        (add-edge! g 4 7)
+        (add-edge! g 4 10)
+        (add-edge! g 10 5)
+        (add-edge! g 5 6)
+        (add-edge! g 10 11)
+        (add-edge! g 11 8)
+        (add-edge! g 8 9)
+        (add-edge! g 11 7)
         g))
 
     ;; (functionaliteit voor de simulator)
@@ -59,35 +66,47 @@
             (aanwezige-treinen (spoor 'aanwezige-treinen)))
         ((aanwezige-treinen 'snelheid-trein) id-symbol)))
 
+    ;; berekent een traject voor trein met 'id' naar 'destination'
     (define (bereken-traject id destination)
-      
+      (let* ((block (((spoor 'aanwezige-treinen) 'detectieblok-trein) id))
+             (from (geef-vector-index block))
+             (to (geef-vector-index destination)))
+        (shortest-path spoornetwerk from to)))
 
-      (define (geef-aanwezige-treinen)
-        (spoor 'aanwezige-treinen))
+    (define (geef-vector-index detectieblok)
+      (let iter
+        ((idx 0))
+        (when (not (>= idx (vector-length componenten)))
+          (if (eq? detectieblok (vector-ref componenten idx))
+              idx
+              (iter (+ idx 1))))))
 
-      (define (geef-trein-ids)
-        (map (lambda (el) (el 'trein-id))
-             ((spoor 'aanwezige-treinen) 'reeks)))
+    (define (geef-aanwezige-treinen)
+      (spoor 'aanwezige-treinen))
 
-      (define (geef-wissel-ids)
-        (spoor 'wissel-ids))
+    (define (geef-trein-ids)
+      (map (lambda (el) (el 'trein-id))
+           ((spoor 'aanwezige-treinen) 'reeks)))
 
-      (define (geef-detectieblok-ids)
-        (spoor 'detectieblok-ids))
+    (define (geef-wissel-ids)
+      (spoor 'wissel-ids))
 
-      (define (verander-wisselstand! id stand)
-        ((spoor 'wijzig-stand-switch!) id stand))
+    (define (geef-detectieblok-ids)
+      (spoor 'detectieblok-ids))
 
-      (define (dispatch-nmbs msg)
-        (cond ((eq? msg 'zet-trein-op-spoor) zet-trein-op-spoor!)
-              ((eq? msg 'verhoog-snelheid-trein!) verhoog-snelheid-trein!)
-              ((eq? msg 'verlaag-snelheid-trein!) verlaag-snelheid-trein!)
-              ((eq? msg 'geef-snelheid-trein) geef-snelheid-trein)
-              ((eq? msg 'bereken-traject) bereken-traject)
-              ((eq? msg 'geef-wissel-ids) geef-wissel-ids)
-              ((eq? msg 'geef-aanwezige-treinen) geef-aanwezige-treinen)
-              ((eq? msg 'geef-trein-ids) geef-trein-ids)
-              ((eq? msg 'geef-detectieblok-ids) geef-detectieblok-ids)
-              ((eq? msg 'verander-wisselstand!) verander-wisselstand!)
-              (else (display "foute boodschap - INFRABEL"))))
-      dispatch-nmbs))
+    (define (verander-wisselstand! id stand)
+      ((spoor 'wijzig-stand-switch!) id stand))
+
+    (define (dispatch-nmbs msg)
+      (cond ((eq? msg 'zet-trein-op-spoor) zet-trein-op-spoor!)
+            ((eq? msg 'verhoog-snelheid-trein!) verhoog-snelheid-trein!)
+            ((eq? msg 'verlaag-snelheid-trein!) verlaag-snelheid-trein!)
+            ((eq? msg 'geef-snelheid-trein) geef-snelheid-trein)
+            ((eq? msg 'bereken-traject) bereken-traject)
+            ((eq? msg 'geef-wissel-ids) geef-wissel-ids)
+            ((eq? msg 'geef-aanwezige-treinen) geef-aanwezige-treinen)
+            ((eq? msg 'geef-trein-ids) geef-trein-ids)
+            ((eq? msg 'geef-detectieblok-ids) geef-detectieblok-ids)
+            ((eq? msg 'verander-wisselstand!) verander-wisselstand!)
+            (else (display "foute boodschap - INFRABEL"))))
+    dispatch-nmbs))
